dbda83f0b389b435f15251a6d659ab1273cfcdc8
charliedodds <charlesgdodds@hotmail.co.uk>
2021-03-29 11:29:15 +0100

    Handle special characters

--- index.js
+++ index.js
@@ -45,11 +45,17 @@ const output = document.querySelector('.output');
 const handleSubmit = (e) => {
 	e.preventDefault();
 	const arrayOfLetters = input.value.split('').map((letter) => letter.toUpperCase());
-	output.innerText = arrayOfLetters
+	const convertedToMorse = arrayOfLetters
 		.map((letter) => {
+			if (alphanumbers.includes(letter)) {
+				return morsecode[alphanumbers.indexOf(letter)];
+			} else if (letter === ' ') {
+				return '/';
+			} else return letter;
 			return letter === ' ' ? '/' : morsecode[alphanumbers.indexOf(letter)];
 		})
 		.join(' ');
+	output.innerText = convertedToMorse;
 };
 
 form.addEventListener('submit', handleSubmit);
